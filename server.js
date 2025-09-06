60661ad1d3a1bdb7bf3d3d723b3f85b1ca16dd9b
CKMatsika <clemie90@gmail.com>
2025-09-06 15:45:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,25 +8,21 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-const contactsRouter = require('./routes/contacts');
-app.use('/api/contacts', contactsRouter);
-
-
-// Connect to MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB Atlas connected'))
-.catch(err => console.error(err));
 
 // Routes
 const contactsRouter = require('./routes/contacts');
 app.use('/api/contacts', contactsRouter);
 
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Atlas connected'))
+  .catch(err => console.error(err));
+
+// Root route
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
